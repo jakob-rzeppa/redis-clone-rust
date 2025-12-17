d42c2ca668f42ae003da74730357c43855bd67c1
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-12-17 15:43:12 +0100

    create a main.rs listening to a tcp socket, extracting a mini-redis connection and responding with a unimplemented error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+
+#[tokio::main]
+async fn main() {
+    // panics if bind fails
+    let listener = TcpListener::bind("127.0.0.1:6379").await.expect("bind failed");
+
+    loop {
+        // accept connections and pass TcpStream to handle_connection
+        let (socket, _) = match listener.accept().await {
+            Ok(res) => res,
+            Err(e) => {
+                println!("accept failed: {}", e);
+                continue;
+            }
+        };
+
+        handle_connection(socket).await;
+    }
+}
+
+async fn handle_connection(socket: TcpStream) {
+    // The `Connection` lets us read/write redis **frames** instead of
+    // byte streams. The `Connection` type is defined by mini-redis.
+    let mut connection = Connection::new(socket);
+
+    match connection.read_frame().await {
+        Ok(Some(frame)) => {
+            println!("GOT: {:?}", frame);
+
+            // Respond with an error
+            let response = Frame::Error("unimplemented".to_string());
+            connection.write_frame(&response).await.unwrap();
+        }
+        _ => {
+            println!("something went wrong when reading frame");
+        }
+    }
 }
