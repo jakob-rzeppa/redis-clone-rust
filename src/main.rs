cb34fa2369f9746f4594c20352b26f9edec437b8
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-12-17 16:26:22 +0100

    handle GET and SET commands in client specific database

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+use anyhow::Result;
 use tokio::net::{TcpListener, TcpStream};
-use mini_redis::{Connection, Frame};
+use mini_redis::{Command, Connection, Frame};
 
 #[tokio::main]
 async fn main() {
@@ -26,20 +28,75 @@ async fn main() {
 }
 
 async fn handle_connection(socket: TcpStream) {
+    use std::collections::HashMap;
+
+    // store data in hashmap
+    let mut db: HashMap<String, Vec<u8>> = HashMap::new();
+
     // The `Connection` lets us read/write redis **frames** instead of
     // byte streams. The `Connection` type is defined by mini-redis.
     let mut connection = Connection::new(socket);
 
-    match connection.read_frame().await {
-        Ok(Some(frame)) => {
-            println!("GOT: {:?}", frame);
+    loop {
+        // get the frame and exit the loop if connection closed or something goes wrong
+        match connection.read_frame().await {
+            Ok(Some(frame)) => {
+                println!("GOT: {:?}", frame);
 
-            // Respond with an error
-            let response = Frame::Error("unimplemented".to_string());
-            connection.write_frame(&response).await.unwrap();
+                let command = match Command::from_frame(frame) {
+                    Ok(c) => c,
+                    Err(e) => {
+                        println!("invalid command: {:?}", e);
+                        continue;
+                    }
+                };
+
+                let response_frame = create_response(command, &mut db).await.unwrap_or_else(|e| {
+                    Frame::Error(e.to_string())
+                });
+
+                // Write the response to the client
+                match connection.write_frame(&response_frame).await {
+                    Ok(_) => {
+                        println!("SENT: {:?}", response_frame);
+                    }
+                    Err(e) => {
+                        println!("sending response failed: {:?}", e);
+                    }
+                };
+            }
+            Ok(None) => {
+                println!("connection closed");
+                break;
+            }
+            _ => {
+                println!("something went wrong when reading frame");
+                break;
+            }
         }
-        _ => {
-            println!("something went wrong when reading frame");
+    }
+}
+
+async fn create_response(command: Command, db: &mut HashMap<String, Vec<u8>>) -> Result<Frame> {
+    use mini_redis::Command::{Get, Set};
+
+    match command {
+        Set(cmd) => {
+            db.insert(cmd.key().to_string(), cmd.value().to_vec());
+            Ok(Frame::Simple("OK".to_string()))
+        },
+        Get(cmd) => {
+            if let Some(value) = db.get(cmd.key()) {
+                // `Frame::Bulk` expects data to be of type `Bytes`. This
+                // type will be covered later in the tutorial. For now,
+                // `&Vec<u8>` is converted to `Bytes` using `into()`.
+                Ok(Frame::Bulk(value.clone().into()))
+            } else {
+                Ok(Frame::Null)
+            }
+        },
+        cmd => {
+            Err(anyhow::Error::msg("command not found"))
         }
     }
 }
