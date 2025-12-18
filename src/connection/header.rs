d748bf2acdcb0cb8d8cf68e068f4b78a164ab8ea
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-12-18 18:44:46 +0100

    create header module to read the request header data

---
+++ src/connection/header.rs
@@ -0,0 +1,57 @@
+use anyhow::Context;
+use tokio::io::{AsyncReadExt, BufReader};
+use tokio::net::TcpStream;
+use crate::connection::Command;
+use crate::connection::metadata::extract_metadata;
+
+pub(super) enum HeaderData {
+    Get { id: u32 },
+    Set { id: u32, content_length: u16 },
+    Insert { content_length: u16 },
+    Remove { id: u32 },
+}
+
+pub(super) async fn read_header(reader: &mut BufReader<TcpStream>) -> Result<HeaderData, anyhow::Error> {
+    // get metadata from the reader
+    let metadata = reader.read_u8().await
+        .context("read metadata failed")?;
+
+    // extract the version and command from the metadata
+    let (version, command) = extract_metadata(&metadata)
+        .context("parse metadata failed")?;
+
+    // only version 1 is implemented
+    if version != 1 {
+        return Err(anyhow::Error::msg("wrong version"))
+    }
+
+    // extract metadata for each command
+    match command {
+        Command::Get => {
+            let id = reader.read_u32().await
+                .context("read id failed")?;
+
+            Ok(HeaderData::Get { id })
+        }
+        Command::Set => {
+            let id = reader.read_u32().await
+                .context("read id failed")?;
+            let content_length = reader.read_u16().await
+                .context("read content_length failed")?;
+
+            Ok(HeaderData::Set { id, content_length })
+        }
+        Command::Insert => {
+            let content_length = reader.read_u16().await
+                .context("read content_length failed")?;
+
+            Ok(HeaderData::Insert { content_length })
+        }
+        Command::Remove => {
+            let id = reader.read_u32().await
+                .context("read id failed")?;
+
+            Ok(HeaderData::Remove { id })
+        }
+    }
+}
